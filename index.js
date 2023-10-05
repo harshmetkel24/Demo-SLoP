cbb2bbf726d851664183b0c648f248576a4d4103
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-10-05 16:38:52 +0530

    task add functionality added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello, World! My First To Do app");
+// Select elements from the DOM
+const taskInput = document.getElementById("task");
+const addButton = document.getElementById("add");
+const taskList = document.getElementById("taskList");
+
+// Add task to the list
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.innerHTML = `
+            <span>${taskText}</span>
+            <button class="delete">Delete</button>
+        `;
+    taskList.appendChild(li);
+    taskInput.value = "";
+  } else {
+    alert("Task cannot be empty");
+  }
+}
+
+// Event listener for adding a task
+addButton.addEventListener("click", addTask);
